eab63f7dab955f1b9340b917667f08214746d93e
Revankar <crevankar@dxc.com>
2019-11-25 19:57:21 +0530

    some text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { AddpostsComponent } from './addposts/addposts.component';
+import { ViewpostsComponent } from './viewposts/viewposts.component';
+import { UsersAppliedComponent } from './users-applied/users-applied.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { UserHomeComponent } from './user-home/user-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminhomeComponent,
+    AddpostsComponent,
+    ViewpostsComponent,
+    UsersAppliedComponent,
+    UserprofileComponent,
+    UserHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
